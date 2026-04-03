f712c0dcd5ef60954a4e1ba8c0d2893ba4454248
Deepak verma <deepakverma726800@gmail.com>
2026-04-03 15:12:49 +0530

    done

--- ORSProject-10/src/main/java/com/rays/ORSProject10Application.java
+++ ORSProject-10/src/main/java/com/rays/ORSProject10Application.java
@@ -49,11 +49,11 @@ public class ORSProject10Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+               registry.addMapping("/**")
+        .allowedOriginPatterns("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
             }
         };
 
